2d35d530b27e06fcafb69e8b75ca1e1ee6e79a37
jt <jt@j.com>
2018-12-20 10:46:40 +0800

    update IPs.py

--- IPs.py
+++ IPs.py
@@ -1,31 +1,52 @@
 import struct
 import socket
 
-class IP:
+class IP(object):
     ip0 = 0
     ip1 = 0
     ipi = ip0
+    bNull = False
+    
     def __init__(self, ips):
-        if '/' in ips:
-            net_addr,mask = ips.split('/')
-            mask = int(mask)
-            start, = struct.unpack('!L', socket.inet_aton(net_addr))
-            start &= 0xFFFFFFFF << (32-mask)
-            end = start | ( 0xFFFFFFFF >> mask )
-            self.ip0 = start
-            self.ip1 = end
-            self.ipi = self.ip0
-        elif '-' in ips:
-            net_addr,max_ipd = ips.split('-')
-            max_ipd = int(max_ipd)
-            start, = struct.unpack('!L', socket.inet_aton(net_addr))
-            end = ( start & 0xFFFFFF00 ) | max_ipd
-            self.ip0 = start
-            self.ip1 = end
-            self.ipi = self.ip0
+        try:
+            if '/' in ips:
+                #192.168.0.101/29
+                net_addr,mask = ips.split('/')
+                mask = int(mask)
+                start, = struct.unpack('!L', socket.inet_aton(net_addr))
+                start &= 0xFFFFFFFF << (32-mask)
+                end = start | ( 0xFFFFFFFF >> mask )
+                self.ip0 = start
+                self.ip1 = end
+                self.ipi = self.ip0
+            elif '-' in ips:
+                net_addr,max_ipd = ips.split('-')
+                if '.' not in max_ipd:
+                    #192.168.0.97-101
+                    max_ipd = int(max_ipd)
+                    start, = struct.unpack('!L', socket.inet_aton(net_addr))
+                    end = ( start & 0xFFFFFF00 ) | max_ipd
+                    self.ip0 = start
+                    self.ip1 = end
+                    self.ipi = self.ip0
+                else:
+                    #192.168.2.97-192.168.2.101
+                    start, = struct.unpack('!L', socket.inet_aton(net_addr))
+                    end, = struct.unpack('!L', socket.inet_aton(max_ipd))
+                    self.ip0 = start
+                    self.ip1 = end
+                    self.ipi = self.ip0
+            if self.ip0 > self.ip1:
+                self.bNull = True
+        except Exception as e:
+            print(e)
+            self.bNull = True
         
     def __iter__(self):
-        return self
+        if self.bNull:
+            return None
+        else:
+            return self
         
     def __next__(self):
         return self.next()
@@ -36,5 +57,5 @@ class IP:
             self.ipi+=1
             return val
         else:
-             raise StopIteration
+            raise StopIteration
         
--- test.py
+++ test.py
@@ -9,3 +9,12 @@ ips = IP('192.168.0.97-101')
 for _ in ips:
     print(_)
     
+print('-'*16)
+ips = IP('192.168.2.97-192.168.2.101')
+for _ in ips:
+    print(_)
+    
+print('-'*16)
+ips = IP('192.168.3.0-192.168.3.0')
+for _ in ips:
+    print(_)
